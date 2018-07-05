d50c6ee8bc1b16d4b68aab35017f292e34943f13
Diogo Cezar <diogo@diogocezar.com>
2018-07-05 08:55:28 -0300

    Add JQuery and Bootstrap

--- js/common/common.js
+++ js/common/common.js
@@ -1,8 +1,12 @@
 import { Util }       from '../modules/util'
 import { Validation } from '../modules/validation'
+import $              from 'jquery'
+import 'popper.js'
+import 'bootstrap'
 
 window.util       = new Util()
 window.validation = new Validation()
+window.$          = $
 
 class Common {
     constructor() {
